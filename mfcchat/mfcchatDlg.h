3231efbfb4c418d41c85231371226a89c02259a7
DingChengMing <7951133@qq.com>
2022-01-31 22:36:07 +0800

    添加项目文件。

---
+++ mfcchat/mfcchatDlg.h
@@ -0,0 +1,34 @@
+﻿
+// mfcchatDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CmfcchatDlg 对话框
+class CmfcchatDlg : public CDialogEx
+{
+// 构造
+public:
+	CmfcchatDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCCHAT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
---
+++ mfcchat/resource.h
@@ -0,0 +1,33 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 mfcchat.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCCHAT_DIALOG              102
+#define IDR_MAINFRAME                   128
+#define IDC_SENGMSG                     1000
+#define IDC_CLEARMSG_BTN                1001
+#define IDC_DISCONNECT                  1002
+#define IDC_CONNECT                     1003
+#define IDC_EDIT1                       1004
+#define IDC_EDIT2                       1005
+#define IDC_EDIT3                       1006
+#define IDC_COMBO1                      1007
+#define IDC_LIST1                       1008
+#define IDC_EDIT4                       1009
+#define IDC_RADIO1                      1010
+#define IDC_EDIT5                       1011
+#define IDC_IPADDRESS1                  1011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
